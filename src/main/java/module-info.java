b6811592cc862ebdd08db6299a89f3edb2d63667
orjuela <orjuela@MateLinux643.myguest.virtualbox.org>
2024-04-14 22:58:30 -0500

    create login view

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires kotlin.stdlib;
+    exports org.example;
+}
---
+++ src/main/java/org/example/Main.kt
@@ -0,0 +1,18 @@
+package org.example
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.layout.VBox
+import javafx.stage.Stage
+
+class Main: Application() {
+    override fun start(primaryStage: Stage?) {
+        val root = FXMLLoader.load<Parent>(javaClass.classLoader.getResource("Login.fxml"))
+        primaryStage?.run {
+            scene = Scene(root)
+            show()
+        }
+    }
+}
